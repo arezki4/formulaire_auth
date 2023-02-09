1134456af2e69e5983fc778c54d3a65e1b37ffee
arezki4 <djaroudarezki@gmail.com>
2023-02-09 20:51:24 +0100

    commit de correction

--- lib/db.php
+++ lib/db.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-	$db = new PDO('mysql:host=localhost;dbname=Formulaire_Securise','root','@A1r2e4z5k0i6');
+	$db = new PDO('mysql:host=localhost;dbname=Formulaire_Securise','root','');
 	$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }catch(Exception $e){
